f3c5af22dd208e1f6f4a4d4d7362aaf70e85a338
William Chang <william.chang@flipp.com>
2024-09-04 10:15:09 -0400

    Fixed typo.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "dmv-sdk",
+            name: "dvm-sdk",
             targets: ["dvm-sdk-binary"]),
     ],
     targets: [
